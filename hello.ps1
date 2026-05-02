6ffa7bfb89328b0cd4513702b920ef6a86de0bbb
Stanley Zhu <stanleyzwj@gmail.com>
2026-05-02 15:37:27 +0800

    Add my first PowerShell hello script

--- hello.ps1
+++ hello.ps1
@@ -6,7 +6,6 @@ Write-Host "Hello from Git + GitHub + PowerShell!" -ForegroundColor Cyan
 Write-Host "Computer : $env:COMPUTERNAME" -ForegroundColor Yellow
 Write-Host "User     : $env:USERNAME" -ForegroundColor Green
 Write-Host "Time     : $(Get-Date)" -ForegroundColor Magenta
-
 Write-Host "`nTop 3 Memory Hogs:" -ForegroundColor Red
 Get-Process | Sort-Object WorkingSet -Descending |
     Select-Object -First 3 Name, @{N='MemoryMB';E={[math]::Round($_.WorkingSet/1MB,1)}}
